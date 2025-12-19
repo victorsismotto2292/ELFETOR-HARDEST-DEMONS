b0605de65b3b8405023f7ff4b8be8eb89460a1fc
Vecyn_SisMoTTO <vgr.sismotto@gmail.com>
2025-12-19 15:37:57 -0300

    Extended list sem o footer
    saber mais ou menos

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-const port = 3010;
+const port = 3030;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -14,6 +14,10 @@ app.use('/img', express.static('img'));
 const MainlevelsPath = path.join(__dirname, 'levels_main.json');
 const MainlevelsData = fs.readFileSync(MainlevelsPath);
 const Mainlevels = JSON.parse(MainlevelsData);
+
+const ExtendedlevelsPath = path.join(__dirname, 'levels_extended.json');
+const ExtendedlevelsData = fs.readFileSync(ExtendedlevelsPath);
+const Extendedlevels = JSON.parse(ExtendedlevelsData);
 
 function CreateCardLevels_Main(level_main, index){
     const position = index + 1;
@@ -85,12 +89,70 @@ function CreateCardLevels_Main(level_main, index){
     }
 }
 
+function CreateCardLevels_Extended(level_extended, index){
+    const position = index + 76; // Starting from 76
+    const videoId = level_extended.video_url.split('v=')[1]?.split('&')[0];
+    const imageSrc = videoId ? `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg` : '/img/placeholder.png';
+    const difficulty = `${level_extended.diff_scale}`;
+    if(level_extended.pos_aredl === "" || level_extended.pos_aredl === 0){
+        let levelCardHtml = `
+                <div class="d-flex justify-content-center">
+                <div class="card mb-3" style="max-width: 1000px; margin: 20px;">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <a href="${level_extended.video_url}" target="_blank"><img src="${imageSrc}" class="img-fluid rounded-start" alt="${level_extended.lvl_name}"></a>
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title" style="font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif; font-size: 3rem; color: #980000;">${position}- ${level_extended.lvl_name} by ${level_extended.lvl_creator}</h5>
+                                <p class="card-text" style="font-weight: 500; font-size: 14px; color: #980000;">(${level_extended.diff_rank})</p>
+                                <p class="card-text" style="font-weight: bold; font-size: small; color: black; margin-bottom: 60px;">Tier (AREDL): ${difficulty}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+                `;
+
+                return levelCardHtml;
+    }
+    else{
+        let levelCardHtml = `
+                <div class="d-flex justify-content-center">
+                <div class="card mb-3" style="max-width: 1000px; margin: 20px;">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <a href="${level_extended.video_url}" target="_blank"><img src="${imageSrc}" class="img-fluid rounded-start" alt="${level_extended.lvl_name}"></a>
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title" style="font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif; font-size: 3rem; color: #980000;">${position}- ${level_extended.lvl_name} by ${level_extended.lvl_creator}</h5>
+                                <p class="card-text" style="font-weight: 500; font-size: 14px; color: #980000;">(Top ${level_extended.pos_aredl} ${level_extended.diff_rank})</p>
+                                <p class="card-text" style="font-weight: bold; font-size: small; color: black; margin-bottom: 60px;">Tier (AREDL): ${difficulty}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+
+        return levelCardHtml;
+    }
+}
+
+
 app.get('/home', (req, res) => {
-    const cardsHtml = Mainlevels.map((level_main, index) => CreateCardLevels_Main(level_main, index)).join('');
     const htmlPagePath = path.join(__dirname, '/public/home.html');
     let htmlPage = fs.readFileSync(htmlPagePath, 'utf-8');
-    htmlPage = htmlPage.replace('{{cardsHtml}}', cardsHtml);
-    res.send(htmlPage);
+    
+    // MAIN LEVELS DATA:
+    const cardsMainHtml = Mainlevels.map((level_main, index) => CreateCardLevels_Main(level_main, index)).join('');
+
+    // EXTENDED LEVELS DATA:
+    const cardsExtendedHtml = Extendedlevels.map((level_extended, index) => CreateCardLevels_Extended(level_extended, index)).join('');
+
+    htmlPage = htmlPage.replace('{{cardsMainHtml}}', cardsMainHtml);
+    res.send(htmlPage + cardsExtendedHtml);
 });
 
 app.listen(port, () => {
